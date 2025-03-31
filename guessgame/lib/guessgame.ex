db7267b7d7140c8f68721e16add209fb357d6e3d
Eugene Koech <eugenekoech9@gmail.com>
2025-03-31 15:02:35 +0300

    implementation using recursion

--- guessgame/lib/guessgame.ex
+++ guessgame/lib/guessgame.ex
@@ -20,21 +20,47 @@ defmodule Guessgame do
 
   def main do
     correct = :rand.uniform(11) - 1
-    guess = IO.gets("Guess numbver between 0 and 10: ") |> String.trim() |> Integer.parse()
-
-    case guess do
-      {result, ""} ->
-        IO.puts("Parse success full #{result}")
-
-        if result == correct do
-          IO.puts("YOU WIN!!")
-        else
-          # {guess, ""} = guess
-          IO.puts("YOU LOSE!! the correct answer was #{correct}")
-        end
-
-      :error ->
-        IO.puts("Something went wrong ")
-    end
+    guess_loop(correct,3)
   end
+    # guess = IO.gets("Guess numbver between 0 and 10: ") |> String.trim() |> Integer.parse()
+
+    # case guess do
+    #   {result, ""} ->
+    #     IO.puts("Parse success full #{result}")
+
+    #     if result == correct do
+    #       IO.puts("YOU WIN!!")
+    #     else
+    #       # {guess, ""} = guess
+    #       IO.puts("YOU LOSE!! the correct answer was #{correct}")
+    #     end
+
+    #   :error ->
+    #     IO.puts("Something went wrong ")
+    # end
+
+    # using recursive programming
+      # this is the base case
+    defp guess_loop(correct,0) do
+      IO.puts("YOU LOSE!! The correct number was #{correct}.")
+
+    end
+
+    defp guess_loop(correct, attempt) do
+      guess = IO.gets("Guess number between 0 and 10: ") |> String.trim() |> Integer.parse()
+      IO.inspect(guess)
+
+      case guess do
+        {result, ""} when result == correct ->
+          IO.puts("YOU WIN!!")
+
+        {result, _} ->
+          IO.puts("Wrong guess. Tries left: #{attempt - 1}")
+          guess_loop(correct, attempt - 1)
+
+        :error ->
+          IO.puts("Invalid input. Please enter a number.")
+          guess_loop(correct, attempt)
+      end
+    end
 end
