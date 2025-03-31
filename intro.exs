b5fdac868e19cf3d06da9f644dbc99827d5d0f8f
Eugene Koech <eugenekoech9@gmail.com>
2025-03-31 10:10:25 +0300

    elixir

---
+++ intro.exs
@@ -0,0 +1,15 @@
+IO.puts("Hello world")
+# .exs - interpreted code
+#  .ex - compiled code
+
+# mix - comppline tool for elixir more like npm in nodejs
+
+# ways of runnig elixir code - using iex (interactive terminal), elixir file only, and elixir project.
+
+# Code is executed when complied and not when run??
+
+# Tests are written in .exs file for them not to be compiled
+
+# data types in elixir - intergers(1245), floats(1.2)->64 bit, strings("inserted with double quotese" ->utf-8 encoded), Booleans(true,false), tuples({1,"hello", :atom, false}), atoms(:atom), Lists,implemented as linked links([1,2,3,4]->first item is the head and the rest are tail)
+
+# String functions - length, upcase,downcase,trim,split
